3cfc264d75ef38af6551e4a65fec256b81633012
Ed Rodrigues <ed.ufpe@gmail.com>
2026-06-24 08:43:12 -0300

    fix: remove concurrency property from serve function payload

--- supabase/functions/render-video/index.ts
+++ supabase/functions/render-video/index.ts
@@ -257,7 +257,6 @@ serve(async (req) => {
       type: "start",
       version: "4.0.482",
       framesPerLambda: 45,
-      concurrency: null,
       concurrencyPerLambda: 1,
       rendererFunctionName: null,
       composition: "Retrospectiva",
